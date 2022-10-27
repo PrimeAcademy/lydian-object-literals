f59f83577ab09969b6e3a2dac749eb12d06a7595
Kris Szafranski <kris@thoughtshelter.com>
2022-10-26 21:13:40 -0500

    end of lecture, for loop in a function

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 let bookList = [
-  {
+  { // object literal
+    // index 0
     title: 'The Race',
     genre: 'Action'
   },
-  {
+  { // object literal
+    // index 1
     title: 'The Journey',
     genre: 'Adventure'
   },
@@ -52,19 +54,43 @@ let bookList = [
     genre: 'Adventure'
   }
 ];
-// You shouldn't need to change anything above this line initially
-// You may change it to test different scenarios, but it should work as is
-
-function bookFinder(selectedGenre) {
-  // Your code goes here
-  // You shouldn't have any console logs in here unless you want to use them for testing
-  let booksInGenre = [];
 
 
+// be able to find all the books in the bookList of a particular genre and put them in a different list
 
-  return booksInGenre;
-}
+// loop thru the books
+
+
+function findBook(findGenre) {
+  // let findGenre = 'Biography';
+  // invent another shelf
+  let matchingBooks = [];
+
+  // go thru each book on the shelf
+  for (let i = 0; i < bookList.length; i++) {
+    let book = bookList[i]; // i = 0, i = 1, i = 2
+    // bookList[1]
+    // bookList[2]
+    // bookList[3]
+
+    // compare findGenre to book's genre
+    if (findGenre == book.genre) {
+      console.log(book.title);
+      // put it on the other shelf
+      matchingBooks.push(book.title);
+    }
+  } // end of for loop
+
+  console.log('matchingBooks has ', matchingBooks);
+
+  return matchingBooks;
+
+
+} // end of findBook
+
+
 
 // Your function should return an arry of the book titles in the selected genre
-console.log('Adventure Books', bookFinder('Adventure'));
-console.log('Biography Books', bookFinder('Biography'));
+console.log('Adventure Books', findBook('Adventure'));
+console.log('Biography Books', findBook('Biography'));
+console.log('Biography Books', findBook('SciFi'));
